c980c517a3e6918b9da907bd68c49103b784ff49
inthrakumar <inthrak04@gmail.com>
2024-06-06 17:24:14 +0530

    create post_form component

---
+++ src/components/post-form/Post_form.jsx
@@ -0,0 +1,18 @@
+import React,{useCallback} from 'react'
+import { useForm } from 'react-hook-form'
+import Button from "../Button";
+import Input from "../Input"
+import Real_time_editor from '../Real_time_editor';
+import Select from '../Select';
+import Database_Service_Object from '../../appwrite/data_config';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+function Post_form({post}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Post_form
