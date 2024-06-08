c64856669b2b62dbecee25c441a6e6902b46298a
inthrakumar <inthrak04@gmail.com>
2024-06-08 12:27:49 +0530

    Edit Add post Edit post

--- src/pages/Addpost.jsx
+++ src/pages/Addpost.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Container from '../components/container/Container'
+import Post_form from '../components/post-form/Post_form'
 function Addpost() {
   return (
-    <div>
+    <div className='py-6'>
+      <Container>
+        <Post_form />
+      </Container>
       
     </div>
   )
--- src/pages/Editpost.jsx
+++ src/pages/Editpost.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { useParams,useNavigate } from 'react-router-dom'
+import Database_Service_Object from '../appwrite/data_config';
+import Container from '../components/container/Container';
+import Post_form from "../components/post-form/Post_form"
 
 function Editpost() {
+  const [post,setpost]=useState(null);
+  const {slug}=useParams();
+  const navigate=useNavigate();
+  useEffect(()=>{
+        if(slug){
+          Database_Service_Object.getPostData(slug).then((post)=>{
+            setpost(post);
+          })
+        }else{
+          navigate("/");
+        }
+  },[slug,navigate])
   return (
-    <div>
+    <div className='py-6'>
+      <Container>
+        <Post_form post={post}/>
+      </Container>
       
     </div>
   )
