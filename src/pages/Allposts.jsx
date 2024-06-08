2414a67effa320ad7f556f5e1315d63ae47a0c48
inthrakumar <inthrak04@gmail.com>
2024-06-08 15:22:22 +0530

    create Allposts.jsx

--- src/pages/Allposts.jsx
+++ src/pages/Allposts.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Database_Service_Object from "../appwrite/data_config";
+import Container from "../components/container/Container";
+import PostCard from "../components/PostCard";
 
 function Allposts() {
+  const [posts, setposts] = useState([]);
+  useEffect(() => {
+    Database_Service_Object.getPosts([]).then((res) => {
+      setposts(res.documents);
+    });
+  }, []);
   return (
-    <div>
-      
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Allposts
+export default Allposts;
