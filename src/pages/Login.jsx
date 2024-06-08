ada8c6c47af68383a0b5ed9179b7cb0bb141d53d
inthrakumar <inthrak04@gmail.com>
2024-06-08 15:34:01 +0530

    Add Login Signup Components to Pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from "../components/Login"
 
 function Login() {
   return (
-    <div>
-      
+    <div className='py-8'>
+      <Login />
     </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { Signup as SignupComponent } from '../components'
 function Signup() {
   return (
-    <div>
-      
-    </div>
+    <div className='py-8'>
+      <SignupComponent />
+      </div>
   )
 }
 
