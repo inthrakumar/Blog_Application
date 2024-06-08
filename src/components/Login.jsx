1c620c201d469aeb027426707936a398e45fe362
inthrakumar <inthrak04@gmail.com>
2024-06-08 11:44:47 +0530

    Login.jsx completed with authservice

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,26 +8,28 @@ import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/slice/authSlice";
 
-function Signup() {
+function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [error, setError] = useState(false);
-  const { register, handleSubmit, watch, setValue, control, getValues } =
-    useForm();
-  const user_creation = async (data) => {
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
+  const login = async (data) => {
+    setError("");
     try {
-      const userData = await Authservice.createAccount(data);
-      if (userData) {
+      const session = await Authservice.login(data);
+      if (session) {
         const userData = await Authservice.getAccount();
-        if (userData) dispatch(login({ userData }));
-        navigate("/");
+        if (userData) {
+          dispatch(login({ userData }));
+          navigate("/");
+        }
       }
     } catch (error) {
-      console.log(error.message);
+        console.log(error.message);
     }
   };
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center w-full">
       <div
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
@@ -37,41 +39,36 @@ function Signup() {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign up to create account
+          Sign in to your account
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
-          Already have an account?&nbsp;
+          Don&apos;t have any account?&nbsp;
           <Link
-            to="/login"
+            to="/signup"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
-            Sign In
+            Sign Up
           </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(user_creation)} className="mt-8">
+        <form onSubmit={handleSubmit(login)} className="mt-8">
           <div className="space-y-5">
             <Input
-              {...register("name", { required: true })}
-              label="Full Name : "
-              placeholder="Full Name"
-            />
-            <Input
-              {...register("email", {
-                required: true,
-              })}
               label="Email : "
               placeholder="Email Address"
               type="email"
+              {...register("email", {
+                required: true,
+              })}
             />
             <Input
-              {...register("password", { required: true })}
               label="Password : "
               type="password"
               placeholder="Password"
+              {...register("password", { required: true })}
             />
             <Button type="submit" className="w-full">
-              Create Account
+              Sign in{" "}
             </Button>
           </div>
         </form>
@@ -79,5 +76,4 @@ function Signup() {
     </div>
   );
 }
-
-export default Signup;
+export default Login;
