8077a38348bef71830370e675ce098df243de586
inthrakumar <inthrak04@gmail.com>
2024-09-03 09:59:57 +0530

    bug fix in accessing env variables

--- src/config/conf.js
+++ src/config/conf.js
@@ -1,11 +1,11 @@
 
 
 const conf = {
-    appwriteUrl: String(import.meta.env.REACT_APP_APPWRITE_URL),
-    appwriteProject: String(import.meta.env.REACT_APP_APPWRITE_PROJECT_ID),
-    appwriteDatabase: String(import.meta.env.REACT_APP_APPWRITE_DATABASE_ID),
-    appwriteCollection: String(import.meta.env.REACT_APP_APPWRITE_COLLECTION_ID),
-    appwriteBucket: String(import.meta.env.REACT_APP_APPWRITE_BUCKET_ID),
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProject: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabase: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollection: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucket: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
 
 export default conf;
