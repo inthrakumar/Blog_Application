2bc332915b4ff4a89a6b98fa688db52ab013c01b
inthrakumar <inthrak04@gmail.com>
2024-09-03 09:23:18 +0530

    updating the env details from dotenv to vite env

--- src/config/conf.js
+++ src/config/conf.js
@@ -1,12 +1,11 @@
-import dotenv from  "dotenv"
-dotenv.config();
 
-const conf={
-    appwriteUrl:String(process.env.REACT_APP_APPWRITE_URL),
-    appwriteProject:String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
-    appwriteDatabase:String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
-    appwriteCollection:String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
-    appwriteBucket:String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
+
+const conf = {
+    appwriteUrl: String(import.meta.env.REACT_APP_APPWRITE_URL),
+    appwriteProject: String(import.meta.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabase: String(import.meta.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollection: String(import.meta.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucket: String(import.meta.env.REACT_APP_APPWRITE_BUCKET_ID),
 }
 
 export default conf;
